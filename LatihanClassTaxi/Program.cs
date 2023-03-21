58e0466aea875683b887dbdb14e458fa260800ca
Staenlyyy <staenlymarcell@students.amikom.ac.id>
2023-03-21 12:52:27 +0700

    first commit

---
+++ LatihanClassTaxi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi_Properties taxi = new Taxi_Properties();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            if (taxi.OnDuty == true)
+            {
+                taxi.PickUpPassenger();
+            } else if
+            (taxi.OnDuty == false)
+            {
+                taxi.DropOffPassenger();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanClassTaxi/Taxi_Properties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassTaxi
+{
+    public class Taxi_Properties
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0} ", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+
+        
+        public void PickUpPassenger()
+            {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+            }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
